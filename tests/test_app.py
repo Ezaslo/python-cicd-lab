aa41879126f30e39908eeb579268ed98467c96bc
Enzo Anselmo <enzo.anselmo@gmail.com>
2026-07-16 10:23:36 +0000

    Fix health test

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,4 +7,4 @@ def test_health():
     response = client.get("/health")
 
     assert response.status_code == 200
-    assert response.get_json() == {"status": "down"}
+    assert response.get_json() == {"status": "ok"}
